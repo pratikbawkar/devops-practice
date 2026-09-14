4c000c7cb9fe619712463d5014e2a624d2fa0d37
pratikbawkwar <pratikbawkar33@gmail.com>
2026-09-14 20:21:28 +0530

    learned about providers.tf and used in example3.tf file

---
+++ Terraform/example.tf
@@ -0,0 +1,33 @@
+# AWS providers for multiple regions
+provider "aws" {
+  alias  = "west"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "east"
+  region = "us-east-1"
+}
+
+# EC2 instance in US West
+# resource "aws_instance" "demo-instance-west" {
+#   provider      = aws.west
+#   ami           = "ami-0b6d-------d99"
+#   instance_type = "t2.micro"
+#
+#   tags = {
+#     Name = "TerraformExampleWest"
+#   }
+# }
+
+# S3 bucket in US East
+resource "aws_s3_bucket" "bucket" {
+  provider = aws.east
+  bucket   = "terraform-example-bucket-east"
+}
+
+# S3 bucket ACL
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "authenticated-read"
+}
--- Terraform/example1.tf
+++ Terraform/example1.tf
@@ -1,16 +1,16 @@
-provider "aws" {
-    region = "us-east-1"
+# provider "aws" {
+#     region = "us-east-1"
   
-}
-resource "aws_instance" "demo-instance" {
-    ami           = "ami-0b6d-------d99"
-    instance_type = "t2.micro"
+# }
+# resource "aws_instance" "demo-instance" {
+#     ami           = "ami-0b6d-------d99"
+#     instance_type = "t2.micro"
   
-    tags = {
-      Name = "TerraformExample"
-    }
+#     tags = {
+#       Name = "TerraformExample"
+#     }
 
-## we can specify security group and subnet id if we want to launch the instance in a specific VPC and subnet
-#     vpc_security_group_ids = ["sg----------f43"]
-#     subnet_id              = "subnet-0f32c0-------3b"  
-}
+# ## we can specify security group and subnet id if we want to launch the instance in a specific VPC and subnet
+# #     vpc_security_group_ids = ["sg----------f43"]
+# #     subnet_id              = "subnet-0f32c0-------3b"  
+# }
